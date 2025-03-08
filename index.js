b2d08b1eec88821768928021842d733930a2689c
mamun-al-abdullah <64242146+mamun-al-abdullah@users.noreply.github.com>
2025-03-08 22:44:23 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 crn(`
-<v pof b15 r15 p15 bgozg clzzz ttu>wow</v>
+<v>
+<h1>Click the link to test the sites </h1>
+<a href="./quran">hafezi quran (emdadiya)</a>
+<a href="./t.html">tiny library example</a>
+
+<a href="./drmamun.html"> dr mamun's status</a>
+
+
+<a href="./touch.html">test✅ touch</a>
+
+<a href="./touchScrolled.html">test❌ touch with scrolling page issue</a>
+
+<a href="./touchScrolledSolved.html">test✅ touch with scrolling background container</a>
+
+<a href="./gridUnsolved.html">test❌ grid image sizing issue</a>
+
+<a href="./grid.html">test✅ grid image size</a>
+
+<a href="./app.html">test✅ responsive site</a>
+
+</v>
 `)
