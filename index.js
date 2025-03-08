69a77e74a52bd01ced7a8e431fe52805cb3a86fd
mamun-al-abdullah <64242146+mamun-al-abdullah@users.noreply.github.com>
2025-03-08 23:16:17 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 crn(`
-<v>
+<v bgaau poa i0>
+	<h bgaae clzzz h58 bsm1 ca>
+		<h><mi>menu</mi></h>
+		<h f ttu>title</h>
+		<h><mi>apps</mi></h>
+	</h>
+
+	<v f>
+		<vs>
+				<v h2000 bg="L,az,ez,iz,oz,uz" c>
+
 <h1>Click the link to test the sites </h1>
 <a href="./quran">hafezi quran (emdadiya)</a>
 <a href="./t.html">tiny library example</a>
@@ -18,6 +28,10 @@ crn(`
 <a href="./grid.html">test✅ grid image size</a>
 
 <a href="./app.html">test✅ responsive site</a>
+
+    </v>
+		</vs>
+	</v>
 
 </v>
 `)
