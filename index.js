bd3f859e0821d8cfbb5ef2f85a0b019cd49529db
mamun-al-abdullah <64242146+mamun-al-abdullah@users.noreply.github.com>
2025-03-15 18:57:58 +0600

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ crn(`
 <v bgaau poa i0>
 	<h bgaae clzzz h58 bsm1 ca>
 		<h s58 c><svg s24><path fill=#fff d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"></path></svg></h>
-		<h f ttu>title</h>
+		<h f ttu>mos</h>
 		<h s58 c><svg s24 fill=#fff><path d="M12 22c1.1 0 2-.9 2-2h-4a2 2 0 0 0 2 2zm6-6v-5c0-3.07-1.64-5.64-4.5-6.32V4c0-.83-.67-1.5-1.5-1.5s-1.5.67-1.5 1.5v.68C7.63 5.36 6 7.92 6 11v5l-2 2v1h16v-1l-2-2z"></path></svg></h>
 	</h>
 
