6b672844dad250db254b9aed0a9c747c073c91e6
mamun <mamunidl3200@gmail.com>
2025-03-19 22:39:13 +0600

    offline support

---
+++ quran/service-worker.js
@@ -0,0 +1,53 @@
+const CACHE_NAME = "pwa-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/lib3.css",
+  "/lib3.js",
+  "/localforage.js",
+  "/MaterialIcons-Regular.ttf",
+];
+
+// Install event - Cache assets
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch event - Serve from cache first
+// self.addEventListener("fetch", (event) => {
+//   event.respondWith(
+//     caches.match(event.request).then((response) => {
+//       return response || fetch(event.request);
+//     })
+//   );
+// });
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request).then((networkResponse) => {
+        return caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        });
+      });
+    })
+  );
+});
+
+// Activate event - Cleanup old caches
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    })
+  );
+});
