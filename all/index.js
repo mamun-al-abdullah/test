4f55c17260c07c947155497cbb1ef5e7c7bee4a6
mamun-al-abdullah <64242146+mamun-al-abdullah@users.noreply.github.com>
2025-04-09 20:27:27 +0600

    Update index.js

--- all/index.js
+++ all/index.js
@@ -1,6 +1,7 @@
 crn(`
 <v poa i0 bg="L,a,e,i,o,u">
 <v h100p>
+<svg poa i0 o2 viewBox="0 0 317 317" fill="#fff" stroke="#2196f3" stroke-width="2"><path d="m71 113mNaN 30"/><path d="m61.5 74.5q-3 1 5 11q0 4 6 12q-4 -1 -9 3q0 0 -14 0l2 4q-11 -2 -13 2q3 4 13 1l1 3h-7l-5 14h6q-6 14 -4 18q2 6 11 -2l18 -27c1 -12 18 -6 29 14q-6 -5 -9 0a36 36 0 1 0 16 62q58 12 67 4v23l19 -9a47 47 0 1 0 73 -47q6 2 7 1q-17 -19 -46 -15l-4 -17h12q21 11 7 -25q-2 -4 -5 -7l5 -6l-3 -11l-5 7q-9 -11 -13 -11l3 6l-8 3q3 -1 -5 -12l1 -6q-4 -5 -9 -1q-3 -8 -7 -5q-3 -3 -5 -6q-9 -1 -5 6l5 5q-4 5 7 8q0 4 6 11q-16 -10 -19 1l-2 -3l-19 -4q-11 1 -25 13q-4 9 -6 10c-24 1 -16 -16 -31 -20q-28 -13 -34 -7q-2 -2 -4 0q-1 -1 -3 -1" fill-opacity="0.22" fill="#2196f3"/><path d="m75.5 134.5a28 28 0 1 0 5 -1z" fill="transparent"/><path d="m229.5 160.5a40 40 0 1 0 5 -1z" fill="transparent"/></svg>
 <vs>
 <v poa i0 p15>
 <h1>Click the link to test the sites </h1>
