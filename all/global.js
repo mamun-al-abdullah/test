2d84a0b9e6fce833397c079be080ac9f2437a0bb
mamun-al-abdullah <64242146+mamun-al-abdullah@users.noreply.github.com>
2025-04-19 22:01:43 +0600

    Update global.js

--- all/global.js
+++ all/global.js
@@ -1,18 +1,28 @@
 
+
 const app = (() =>{
 
-crn(`<v poa i0>
+const {main} = crn(`<v poa i0>
 
 <meta name="theme-color" media="(prefers-color-scheme: light)" content="#00a1ff" />
 
-<meta name="theme-color" media="(prefers-color-scheme: dark)" content="#00a1ff" />
+<meta name="theme-color" media="(prefers-color-scheme: dark)" content="#0d1419" />
 
-<v poa i0 bg="L180,oz,wz" c clzzz f50 ttu>m o s</v>
+<v ref=main poa i0 bg="L180,oz,wz" c clzzz f50 ttu>m o s</v>
 
 </v>`)
 
 os.statusBar.hslColor(`bgoz`)
 
+return {
+main
+}
+
 })()
 
 
+
+crn(app.main, `
+<v pof b0 f20 bgo p5 px15 cm15 cn15>
+task
+</v>
