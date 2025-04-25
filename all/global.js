46b5eb55e8282e05712763e0e7f71cd86ca86fcb
mamun-al-abdullah <64242146+mamun-al-abdullah@users.noreply.github.com>
2025-04-25 23:26:32 +0600

    Update global.js

--- all/global.js
+++ all/global.js
@@ -1,40 +1,14 @@
 
 
 const app = (() =>{
-
-var d = new Date();
-
-const mnt = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-
-const dy = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
-
-const getFormattedTime = ()=> {
-  const now = new Date();
-  let hours = now.getHours();
-  const minutes = now.getMinutes();
-
-  const ampm = hours >= 12 ? 'pm' : 'am';
-  hours = hours % 12;
-  hours = hours ? hours : 12;
-  const formattedMinutes = minutes < 10 ? '0' + minutes : minutes;
-
-  return `${hours.toString().padStart(2, '0')}:${formattedMinutes} ${ampm}`;
-}
-
-
-
-const {main} = crn(`<v poa i0 nos>
+const O = crn(`<v poa i0 nos>
 
 <meta name="theme-color" media="(prefers-color-scheme: light)" content="#00a1ff" />
 
 <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#0d1419" />
 
-<v ref=main poa i0 bg="L180,oz,wz" c clzzz>
-<h f24 fb>${getFormattedTime()}</h>
-	<h f19>
-		<h mr15 clnz fb>${dy[d.getDay()]}</h>
-		<h clkz>${d.getDate()} ${mnt[d.getMonth()]}</h>
-	</h>
+<v ref=Main poa i0 bg="L180,oz,wz" c clzzz>
+<v ref=DateTime></v>
 <h f50 fb mt100>M O S</h>
 </v>
 
@@ -42,7 +16,9 @@ const {main} = crn(`<v poa i0 nos>
 
 
 return {
-main
+	Home : {
+		...O
+	}
 }
 
 })()
@@ -58,6 +34,39 @@ task : `<svg fill=#ffffffaa s24><path d="M22 5.18 10.59 16.6l-4.24-4.24 1.41-1.4
 
 book : `<svg fill=#ffffffaa s24><path d="M21 5c-1.11-.35-2.33-.5-3.5-.5-1.95 0-4.05.4-5.5 1.5-1.45-1.1-3.55-1.5-5.5-1.5S2.45 4.9 1 6v14.65c0 .25.25.5.5.5.1 0 .15-.05.25-.05C3.1 20.45 5.05 20 6.5 20c1.95 0 4.05.4 5.5 1.5 1.35-.85 3.8-1.5 5.5-1.5 1.65 0 3.35.3 4.75 1.05.1.05.15.05.25.05.25 0 .5-.25.5-.5V6c-.6-.45-1.25-.75-2-1zm0 13.5c-1.1-.35-2.3-.5-3.5-.5-1.7 0-4.15.65-5.5 1.5V8c1.35-.85 3.8-1.5 5.5-1.5 1.2 0 2.4.15 3.5.5v11.5z"></path><path d="M17.5 10.5c.88 0 1.73.09 2.5.26V9.24c-.79-.15-1.64-.24-2.5-.24-1.7 0-3.24.29-4.5.83v1.66c1.13-.64 2.7-.99 4.5-.99zM13 12.49v1.66c1.13-.64 2.7-.99 4.5-.99.88 0 1.73.09 2.5.26V11.9c-.79-.15-1.64-.24-2.5-.24-1.7 0-3.24.3-4.5.83zm4.5 1.84c-1.7 0-3.24.29-4.5.83v1.66c1.13-.64 2.7-.99 4.5-.99.88 0 1.73.09 2.5.26v-1.52c-.79-.16-1.64-.24-2.5-.24z"></path></svg>`,
 }
+app.Home.getDateTime = () =>{
+var d = new Date();
+const mnt = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+
+const dy = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+
+  let hours = d.getHours();
+  const minutes = d.getMinutes();
+  const seconds = d.getSeconds();
+
+  const ampm = hours >= 12 ? 'pm' : 'am';
+  hours = hours % 12;
+  hours = hours ? hours : 12;
+
+  const formattedMinutes = minutes.toString().padStart(2, '0');
+  const formattedSeconds = seconds.toString().padStart(2, '0');
+  const formattedHours = hours.toString().padStart(2, '0');
+
+  return `
+<h f24 fb>${formattedHours}:${formattedMinutes}:${formattedSeconds} ${ampm}</h>
+	<h f22>
+		<h mr45 clnz fb>${dy[d.getDay()]}</h>
+		<h clkz>${d.getDate()} ${mnt[d.getMonth()]}</h>
+	</h>
+`;
+}
+
+
+app.Home.DateTime.ih = app.Home.getDateTime()
+aa(1000, v=>{
+	app.Home.DateTime.ih = app.Home.getDateTime()
+})
+
 crn( `
 <v pof r0 t50p ty-50p f20 bg="L90,oz,wz" px5 py5 bsm5 cm15 cp15 oh>
 	<v>
